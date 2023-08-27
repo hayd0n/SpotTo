ddec206f9cbdd0adfef33691b51dfbb82b7fd4a0
hayd0n <xanax@yahoo.com>
2023-08-27 22:09:26 +0100

    Initial commit

---
+++ SpotTo/MVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotTo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private SpotifyAPIHelper _spotifyAPIHelper = new SpotifyAPIHelper();
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private async void FetchPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            // Extract playlist ID from URL
+            var playlistId = ExtractPlaylistIdFromUrl(PlaylistUrlTextBox.Text);
+
+            if (!string.IsNullOrEmpty(playlistId))
+            {
+                var songs = await _spotifyAPIHelper.GetSongsFromPlaylist(playlistId);
+                SongsListBox.ItemsSource = songs;
+                SongCountLabel.Content = $"Songs: {songs.Count()}"; // Update the label with song count
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid Playlist URL");
+            }
+
+
+        }
+
+
+        public static string ExtractPlaylistIdFromUrl(string url)
+        {
+            Uri uri;
+
+            if (!Uri.TryCreate(url, UriKind.Absolute, out uri) || uri.Host != "open.spotify.com" || !uri.AbsolutePath.StartsWith("/playlist/"))
+            {
+                throw new ArgumentException("Invalid Spotify playlist URL.");
+            }
+
+            string path = uri.AbsolutePath;
+            string[] parts = path.Split('/');
+            if (parts.Length > 2)
+            {
+                return parts[2];
+            }
+            else
+            {
+                throw new ArgumentException("Invalid Spotify playlist URL.");
+            }
+        }
+
+        private void PlaylistUrlTextBox_TextInput(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ SpotTo/SpotifyAPI.cs
@@ -0,0 +1,78 @@
+﻿    using SpotifyAPI.Web;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+public class SpotifyAPIHelper
+{
+    private readonly string _clientId = "";
+    private readonly string _clientSecret = "";
+    private SpotifyClient _spotifyClient;
+
+    public SpotifyAPIHelper()
+    {
+        ConfigureClient();
+    }
+
+    private void ConfigureClient()
+    {
+        var config = SpotifyClientConfig.CreateDefault();
+        var request = new ClientCredentialsRequest(_clientId, _clientSecret);
+
+        // Use non-generic version
+        var responseTask = new OAuthClient(config).RequestToken(request);
+        var response = responseTask.Result as ClientCredentialsTokenResponse;
+
+        if (response != null)
+        {
+            _spotifyClient = new SpotifyClient(config.WithToken(response.AccessToken));
+        }
+        else
+        {
+            throw new Exception("Failed to obtain Spotify token.");
+        }
+    }
+
+
+    public async Task<List<string>> GetSongsFromPlaylist(string playlistId)
+    {
+        try
+        {
+            var songs = new List<string>();
+            var itemsPerPage = 100;
+            var currentOffset = 0;
+
+            while (true)
+            {
+                var request = new PlaylistGetItemsRequest { Limit = itemsPerPage, Offset = currentOffset };
+                var playlistItems = await _spotifyClient.Playlists.GetItems(playlistId, request);
+
+                if (playlistItems.Items.Count == 0)
+                {
+                    break;
+                }
+
+                foreach (var item in playlistItems.Items)
+                {
+                    if (item.Track is FullTrack track)
+                    {
+                        songs.Add($"{track.Name} - {track.Artists.FirstOrDefault()?.Name}");
+                    }
+                }
+
+                currentOffset += itemsPerPage;
+            }
+
+            return songs;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching songs from playlist: {ex.Message}");
+            return new List<string>();
+        }
+    }
+
+
+
+}
